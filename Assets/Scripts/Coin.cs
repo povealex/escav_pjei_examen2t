51d490f16a4f1a668026c88d5aefcd78a62973b5
juancarlosgdm <juancarlosgdm@gmail.com>
2020-03-10 10:25:37 +0100

    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    [Tooltip("¿Se trata de la moneda final de nivel?")]
+    [SerializeField]
+    private bool finalCoin;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // Comprueba si la moneda ha sido conseguida por el jugador
+        if (other.CompareTag("Player")) {
+            if (!finalCoin) {
+                // Moneda normal, se incrementa el contador
+                ++GameManager.Coins;
+                Destroy(transform.parent.gameObject);
+            } else {
+                // Moneda final, nivel completado
+                GameManager.LevelFinished();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    /* Singleton */
+    private static GameManager instance;
+
+    [Tooltip("Referencia al contador de monedas en la interfaz")]
+    [SerializeField]
+    private Text coinsCounter;
+
+    [Tooltip("Referencia al panel de nivel finalizado")]
+    [SerializeField]
+    private GameObject levelFinishedPanel;
+
+    // Propiedad que almacena el número de monedas recolectadas por el jugador
+    private int coins;
+    public static int Coins {
+        get {
+            return instance.coins;
+        }
+        set {
+            instance.coins = value;
+            instance.coinsCounter.text = value.ToString("00");
+        }
+    }
+
+    #region Unity Messages
+
+    private void Awake() {
+        /* Singleton (se da por hecho que nunca habrá más de una instancia del GameManager) */
+        instance = this;
+    }
+
+    #endregion
+
+    #region Public Static Messages
+
+    public static void LevelFinished() {
+        // Muestra la pantalla de nivel finalizado
+        instance.levelFinishedPanel.SetActive(true);
+        instance.Invoke("LoadGame", 3.0f);
+    }
+
+    #endregion
+
+    private void LoadGame() {
+        // Carga la escena del juego
+        SceneManager.LoadScene(0);
+    }
+}
